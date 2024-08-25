7f45acbe6cb9f98d27abc5668aa59fb2d646b383
Mayuri Lande <er.hajare.mayu@gmail.com>
2024-08-25 11:29:01 +0530

    Changes for data

--- ergoGateWay/src/main/java/com/ubs/hackathon/configuration/EurekaClientConfiguration.java
+++ ergoGateWay/src/main/java/com/ubs/hackathon/configuration/EurekaClientConfiguration.java
@@ -1,27 +1,14 @@
-package com.ubs.hackathon.configuration;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
-import com.netflix.discovery.Jersey3DiscoveryClientOptionalArgs;
-import com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
-import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
-
-@Configuration
-public class EurekaClientConfiguration {
-
-	@Bean
-	@ConditionalOnMissingBean(AbstractDiscoveryClientOptionalArgs.class)
-	 Jersey3DiscoveryClientOptionalArgs jersey3DiscoveryClientOptionalArgs() {
-	    return new Jersey3DiscoveryClientOptionalArgs();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean(TransportClientFactories.class)
-	 Jersey3TransportClientFactories jersey3TransportClientFactories() {
-	    return Jersey3TransportClientFactories.getInstance();
-	}
-	
-}
+/*
+ * package com.ubs.hackathon.configuration;
+ * 
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration;
+ * 
+ * import
+ * com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
+ * 
+ * @Configuration public class EurekaClientConfiguration {
+ * 
+ * @Bean TransportClientFactories<?> transportClientFactories() { return
+ * JerseyEurekaHttpClientFactory.INSTANCE; } }
+ */
--- ergoUserManagementService/src/main/java/com/ubs/hackathon/controller/ErgoUserController.java
+++ ergoUserManagementService/src/main/java/com/ubs/hackathon/controller/ErgoUserController.java
@@ -3,6 +3,7 @@ package com.ubs.hackathon.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import com.ubs.hackathon.model.UserDetails;
 import com.ubs.hackathon.service.ErgoUserService;
 
 import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/ergoBuddy/users")
@@ -59,5 +61,10 @@ public class ErgoUserController {
 	public ResponseEntity<Void> deleteUser(@PathVariable Long gpnID) {
 		ergoUserService.deleteErgoUserById(gpnID);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/healthCheck")
+	public String healthCheck() {
+		return "It is up and running!";
 	}
 }
